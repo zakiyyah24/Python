91ce56437d6f98a2bd7b57dc1322cc03cf0a6806
Zakiyyah Wardah <56468922+zakiyyah24@users.noreply.github.com>
2020-10-17 20:30:23 +0700

    Add files via upload

---
+++ If Bersarang.py
@@ -0,0 +1,13 @@
+usia = int(input("Masukkan Usia: "))
+if usia > 19:
+    print("Selamat Anda Bisa Mendapatkan KTP! ")
+else:
+    print("Maaf, usia anda belum cukup!!")
+    print("Yang Sabar Yaa.. :v ")
+
+passwordwifi = input("Enter a passwordwifi: ")
+if passwordwifi == "Assalamualaikum":
+    print("Welcome to wifi STTGdg1")
+else:
+    print("Correct Your PasswordWifi")
+    print("Please try again")
